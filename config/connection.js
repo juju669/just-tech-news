2f470dcafbe7e92e71062a6d0c3a8f539af6552a
Julian Wilson <58565920+juju669@users.noreply.github.com>
2021-09-14 14:13:33 -0400

    created schema files

---
+++ config/connection.js
@@ -0,0 +1,11 @@
+// import the sequelize constructor from the library 
+const Sequelize = require('sequelize');
+
+//create connection to our database, pass in your MysqL info
+const sequelize =new Sequelize('just_tech_news_db', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
+
+module.exports = sequelize;
